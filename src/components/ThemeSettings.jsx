17021b0e37d086381c01214490de77f1267bc6dc
Eduardo Gonçalves <milbratheduardo@gmail.com>
2023-11-21 01:44:08 -0300

    cor letra themesettings

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -15,7 +15,7 @@ const ThemeSettings = () => {
     <div className='bg-half-transparent w-screen fixed 
     nav-item top-0 right-0'>
       <div className='float-right h-screen
-       dark:text-gray-200 bg-white dark:[#484B52]
+       dark:text-gray-400 bg-white dark:[#484B52]
        w-400'>
         <div className='flex justify-between items-center 
         p-4 ml-4'>
