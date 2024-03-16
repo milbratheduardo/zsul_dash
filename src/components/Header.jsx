1e7771211e91870735159b92c39d0fab79906cf5
Eduardo Gonçalves <milbratheduardo@gmail.com>
2024-03-16 11:16:20 -0300

    sumulas

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 
-const Header = ({category, title }) => {
+const Header = ({ category, title, subtitle }) => {
   return (
     <div className='mb-10'>
-      <p className='text-gray-400'>
-        {category}
-      </p>
-      <p className='text-3xl font-extrabold tracking-tight 
-      text-slate-900'>
-        {title}
-      </p>
+      <p className='text-gray-400'>{category}</p>
+      <p className='text-3xl font-extrabold tracking-tight text-slate-900'>{title}</p>
+      {subtitle && (
+        <p className='text-lg font-semibold text-gray-600'>{subtitle}</p>
+      )}
     </div>
   )
 }
