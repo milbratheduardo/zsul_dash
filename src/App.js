1851242f0019cec4895344495b3e9354f0283a97
Eduardo Gonçalves <milbratheduardo@gmail.com>
2024-03-21 15:27:24 -0300

    reload

--- src/App.js
+++ src/App.js
@@ -14,6 +14,15 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Cabecalho } from './components';
 
+useEffect(() => {
+  const shouldReload = localStorage.getItem('reload');
+  
+  if (shouldReload === '1') {
+    localStorage.removeItem('reload');
+    window.location.reload();
+  }
+}, []);
+
 const App = () => {  
     return (
         <BrowserRouter>
